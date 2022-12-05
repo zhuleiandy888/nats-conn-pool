4dca4c4adc1f5b31dd730d583b65847be5bfa8a0
zhulei <zhulei@thunder.com.cn>
2022-12-05 15:23:55 +0800

    update: example file

--- example/main.go
+++ example/main.go
@@ -3,51 +3,85 @@
  * @Author: zhulei
  * @Date: 2022-12-02 20:33:37
  * @LastEditors: zhulei
- * @LastEditTime: 2022-12-02 21:56:38
+ * @LastEditTime: 2022-12-05 15:21:40
  */
 package main
 
 import (
+	// "fmt"
 	"fmt"
-	"net"
-	"os"
-	"os/signal"
-	"syscall"
+	"math/rand"
+	"strconv"
 	"time"
 
+	"github.com/nats-io/nats.go"
 	pool "github.com/zhuleiandy888/nats-conn-pool"
 )
 
-const addr string = "127.0.0.1:8080"
+const (
+	NATS_SERVER string = "nats://192.168.1.1:4222"
+	// 连接nats-server 超时时间
+	DEFAULT_CONNECT_TIMEOUT int = 1
+	// MAX_RECONNECTS          int    = 3
+	// RECONNECT_WAIT          int    = 3
+	PING_INTERVAL         int    = 20
+	MAX_PINGS_OUTSTANDING int    = 3
+	TOKEN                 string = "xxxxxxxxxx"
+)
 
-func main() {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGUSR1, syscall.SIGUSR2)
-	go server()
-	//等待tcp server启动
-	time.Sleep(2 * time.Second)
-	client()
-	fmt.Println("使用: ctrl+c 退出服务")
-	<-c
-	fmt.Println("服务退出")
+func RandStr(length int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	rand.Seed(time.Now().UnixNano() + int64(rand.Intn(100)))
+	for i := 0; i < length; i++ {
+		result = append(result, bytes[rand.Intn(len(bytes))])
+	}
+	return string(result)
 }
 
 func client() {
 
 	//factory 创建连接的方法
-	factory := func() (interface{}, error) { return net.Dial("tcp", addr) }
+	factory := func() (interface{}, error) {
+		return nats.Connect(NATS_SERVER, nats.Token(TOKEN),
+			nats.Timeout(time.Duration(DEFAULT_CONNECT_TIMEOUT)*time.Second),
+			// nats.RetryOnFailedConnect(true),
+			// nats.MaxReconnects(MAX_RECONNECTS),
+			// nats.ReconnectWait(time.Duration(RECONNECT_WAIT)*time.Second),
+			// nats.ReconnectJitter(time.Second*3, time.Second*3),
+			//到达 MAX_PINGS_OUTSTANDING 次间隔时间（PING_INTERVAL * MAX_PINGS_OUTSTANDING 秒）仍然没有收到回复pong的关闭这个连接
+			nats.PingInterval(time.Duration(PING_INTERVAL)*time.Second),
+			nats.MaxPingsOutstanding(MAX_PINGS_OUTSTANDING),
+			// nats.DisconnectErrHandler(func(nc *nats.Conn, err error) {
+			// 	// handle disconnect error event
+
+			// }),
+			// nats.ReconnectHandler(func(nc *nats.Conn) {
+			// 	// handle reconnect event
+
+			// }),
+			// nats.ClosedHandler(func(nc *nats.Conn) {
+			// 	// handle close event
+
+			// }),
+		)
+	}
 
 	//close 关闭连接的方法
-	close := func(v interface{}) error { return v.(net.Conn).Close() }
+	close := func(v interface{}) error { return v.(*nats.Conn).Drain() }
 
-	//创建一个连接池： 初始化2，最大连接5，空闲连接数是4
+	ping := func(v interface{}) bool { return v.(*nats.Conn).IsConnected() }
+
+	//创建一个连接池： 初始化30，最大连接40
 	poolConfig := &pool.Config{
-		InitialCap: 2,
-		MaxCap:     5,
+		InitialCap: 100,
+		MaxCap:     1000,
 		Factory:    factory,
 		Close:      close,
+		Ping:       ping,
 		//连接最大空闲时间，超过该时间的连接 将会关闭，可避免空闲时连接EOF，自动失效的问题
-		IdleTimeout: 15 * time.Second,
+		IdleTimeout: 600 * time.Second,
 	}
 	p, err := pool.NewChannelPool(poolConfig)
 	if err != nil {
@@ -55,36 +89,66 @@ func client() {
 	}
 
 	//从连接池中取得一个连接
-	v, err := p.Get()
+	// nc, err := p.Get()
 
+	current := p.Len()
+	fmt.Println("len=", current)
+	// time.Sleep(6 * time.Second)
 	//do something
 	//conn=v.(net.Conn)
+	count := 0
+
+	for {
+		count++
+		if count > 10000 {
+			break
+		}
+		if count%1000 == 0 {
+			time.Sleep(1 * time.Second)
+		}
+		go pubMsg(p, "subject_1", []byte("Hello World--"+strconv.Itoa(count)+"--"+RandStr(8)))
+		go pubMsg(p, "subject_2", []byte("Hello World--"+strconv.Itoa(count)+"--"+RandStr(8)))
+		go pubMsg(p, "subject_3", []byte("Hello World--"+strconv.Itoa(count)+"--"+RandStr(8)))
+
+	}
 
 	//将连接放回连接池中
-	p.Put(v)
+	// p.Put(nc)
 
 	//释放连接池中的所有连接
 	//p.Release()
 
 	//查看当前连接中的数量
-	current := p.Len()
+	// current = p.Len()
+	// fmt.Println("len=", current)
+	// time.Sleep(10 * time.Second)
+}
+
+func pubMsg(pl pool.Pool, sub string, msg []byte) {
+	nc, err := pl.Get()
+	if err != nil {
+		fmt.Println("err0=", err)
+	}
+	// current := pl.Len()
+	// fmt.Println("len=", current)
+	err = nc.(*nats.Conn).Publish(sub, msg)
+	if err != nil {
+		fmt.Println("err1=", err)
+	}
+	err = nc.(*nats.Conn).Flush()
+	if err != nil {
+		fmt.Println("err2=", err)
+	}
+
+	pl.Put(nc)
+	current := pl.Len()
 	fmt.Println("len=", current)
 }
 
-func server() {
-	l, err := net.Listen("tcp", addr)
-	if err != nil {
-		fmt.Println("Error listening: ", err)
-		os.Exit(1)
-	}
-	defer l.Close()
-	fmt.Println("Listening on ", addr)
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting: ", err)
-		}
-		fmt.Printf("Received message %s -> %s \n", conn.RemoteAddr(), conn.LocalAddr())
-		//go handleRequest(conn)
-	}
+func main() {
+
+	client()
+
+	time.Sleep(120 * time.Second)
+
 }
